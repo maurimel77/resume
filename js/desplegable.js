0e7df16d4f226f016ae38178bad8f933860398cb
maurimel77 <126414689+maurimel77@users.noreply.github.com>
2023-03-12 19:33:49 -0300

    Update desplegable.js

--- js/desplegable.js
+++ js/desplegable.js
@@ -1,4 +1,5 @@
 
+//Codigo para desplegar el texto de cursos al tocar el boton Ver mas.
 
 let estado = true;
 
@@ -7,12 +8,13 @@ document.getElementById("boton_mostrar").addEventListener('click', function () {
     if (estado)
     {
     document.getElementById("txt_cursos").innerHTML = "Post graduate Course in Embedded Systems (96 hours). Facultad de Ingenieria University of Buenos Aires (FIUBA).  <br>Automata Programming Under IEC 61131-3 International Standard.  Argentina. <br>Design in VHDL for FPGA. National University of San Luis - San Luis - Argentina <br>Functional Verification for Programmable Logic Designs at INVAP Aerospace <br>Lab VIEW 7 at Istituto Nazionale di Fisica Nucleare (INFN) - Milan - Italy <br>Photomultiplier Tubes (PMT) School & SD/FD Calibration Workshop - Malargüe - Argentina <br>Digi University Workshop XBee, ZigBee, iButton at National Technological University";
-    
+    console.log("entro al if de mostrar mas, y estado = " + estado);
     }
 
     else
     {
         document.getElementById("txt_cursos").innerHTML = ""; 
+        console.log("entro al else de mostrar menos, y estado = " + estado);
     }
 
     estado = !estado;
